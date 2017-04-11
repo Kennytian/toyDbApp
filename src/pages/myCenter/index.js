b35c6d6929d8699249a6deaf33442adef14beb2e
Kenny <kt.tian@gmail.com>
2017-04-12 02:03:54 +0800

    Fix indent

--- src/pages/myCenter/index.js
+++ src/pages/myCenter/index.js
@@ -16,7 +16,7 @@ export default class MyCenter extends Component {
   render() {
     return (
       <View style={[styles.container]}>
-        <Text style={[styles.welcome,{margin:0,padding:0}]}>
+        <Text style={[styles.welcome,{margin: 0,padding: 0}]}>
           Welcome to MyCenter!
         </Text>
         <ImageCard source={imageSource.temp_image_card} text={'我的世界游戏周边'} titlePosition={'left'} onPress={()=>{}}/>
