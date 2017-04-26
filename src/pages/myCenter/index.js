e34ea58245d51303686dc092150bb38c9a084218
Kenny <kt.tian@gmail.com>
2017-04-26 21:31:05 +0800

    Adjuest myCenter code

--- src/pages/myCenter/index.js
+++ src/pages/myCenter/index.js
@@ -1,7 +1,7 @@
 /* @flow */
 
 import React, { Component } from 'react';
-import { Image, Text, View, TouchableOpacity } from 'react-native';
+import { Image, ScrollView, Text, View, TouchableOpacity } from 'react-native';
 
 import ImageSource from '../../constants/imageSource';
 import styles from './index.style';
@@ -29,30 +29,32 @@ class MyCenter extends Component {
   render() {
     const {navigate} = this.props.navigation;
     return (
-      <View style={styles.myCenterWrap}>
-        <View style={styles.tittle}>
-          <Text style={styles.center}>个人中心</Text>
-          <TouchableOpacity style={styles.setImage} onPress={() => {
-            alert(1);
-          }}>
-            <Image source={ImageSource.myCenter.set}/>
+      <ScrollView>
+        <View style={styles.myCenterWrap}>
+          <View style={styles.tittle}>
+            <Text style={styles.center}>个人中心</Text>
+            <TouchableOpacity style={styles.setImage} onPress={() => {
+              alert(1);
+            }}>
+              <Image source={ImageSource.myCenter.set}/>
+            </TouchableOpacity>
+          </View>
+          <Image style={styles.headerImg} source={ImageSource.myCenter.headerImg}>
+            <View style={styles.userName}>
+              <Text>用户名</Text>
+            </View>
+          </Image>
+          <View style={styles.main}>
+            {this._renderButton(() => this._navigateTo('DevEntry'), ImageSource.myCenter.message, '开发入口')}
+            {this._renderButton(this.ccc, ImageSource.myCenter.message, '我的消息')}
+            {this._renderButton(this.ccc, ImageSource.myCenter.browsingHistory, '浏览记录')}
+            {this._renderButton(this.ccc, ImageSource.myCenter.clean, '清理缓存')}
+          </View>
+          <TouchableOpacity style={styles.exit}>
+            <Text style={styles.exitText}>退出登录</Text>
           </TouchableOpacity>
         </View>
-        <Image style={styles.headerImg} source={ImageSource.myCenter.headerImg}>
-          <View style={styles.userName}>
-            <Text>用户名</Text>
-          </View>
-        </Image>
-        <View style={styles.main}>
-          {this._renderButton(() => this._navigateTo('DevEntry'), ImageSource.myCenter.message, '开发入口')}
-          {this._renderButton(this.ccc, ImageSource.myCenter.message, '我的消息')}
-          {this._renderButton(this.ccc, ImageSource.myCenter.browsingHistory, '浏览记录')}
-          {this._renderButton(this.ccc, ImageSource.myCenter.clean, '清理缓存')}
-        </View>
-        <TouchableOpacity style={styles.exit}>
-          <Text style={styles.exitText}>退出登录</Text>
-        </TouchableOpacity>
-      </View>
+      </ScrollView>
     );
   }
 }
